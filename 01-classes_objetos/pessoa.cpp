17e85638063c97be5ccc85bcde51d04c98540adc
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-28 15:59:44 -0300

    Adiciona arquivo .gitignore e implementa a classe Pessoa

---
+++ 01-classes_objetos/pessoa.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Definição da classe
+class Pessoa {
+    // Atributos (variáveis de instância)
+    private: // private: só a própria classe pode acessar
+        string nome;
+        char sexo;
+        int idade;
+        bool usaOculos;
+};
+
+int main(){
+    
+    return 0;
+}
