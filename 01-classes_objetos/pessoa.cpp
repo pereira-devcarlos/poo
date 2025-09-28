4dfd5faa9c00593b9fcfdfb1a9a4ac4ba824b6b3
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-28 16:01:17 -0300

    Atualiza comentários na definição da classe Pessoa e organiza a estrutura do código

--- 01-classes_objetos/pessoa.cpp
+++ 01-classes_objetos/pessoa.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 
-// Definição da classe
+// Definição da classe Pessoa
 class Pessoa {
-    // Atributos (variáveis de instância)
+    // Atributos da classe (características)
     private: // private: só a própria classe pode acessar
         string nome;
         char sexo;
         int idade;
         bool usaOculos;
+
+    // Métodos da classe (comportamentos)
+    public: // public: qualquer um pode acessar
+        Pessoa(){  // Construtor (inicializa os atributos)
+            nome = "";
+            sexo = '\0';
+            idade = 0;
+            usaOculos = false;
+        }
 };
 
 int main(){
+    Pessoa p1;
     
     return 0;
 }
