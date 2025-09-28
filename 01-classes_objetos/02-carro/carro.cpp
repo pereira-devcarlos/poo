cc5be0624c58ff5c87fc6374fabdaad85bb0ec90
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-28 17:56:32 -0300

    Remove o código de teste do arquivo carro.cpp e adiciona o arquivo main.cpp com testes da classe Carro

--- 01-classes_objetos/02-carro/carro.cpp
+++ 01-classes_objetos/02-carro/carro.cpp
@@ -37,19 +37,3 @@ int Carro::getAno(){
 string Carro::getCor(){
     return cor;
 }
-
-int main() {
-    Carro car1;
-
-    // Testando os métodos set's da classe carro
-    car1.setModelo("Gol Bola");
-    car1.setAno(2010);
-    car1.setCor("Preto");
-
-    // Testando os métodos get's da classe carro
-    cout << "\nModelo: " << car1.getModelo();
-    cout << "\nAno: " << car1.getAno();
-    cout << "\nCor: " << car1.getCor() << endl; 
-
-    return 0;
-}
