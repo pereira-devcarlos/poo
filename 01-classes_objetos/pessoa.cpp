afec18b5b83693b881c5161a653040c3ec1becff
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-28 16:02:34 -0300

    Refatora a classe Pessoa: organiza métodos e adiciona validação na definição da idade

--- 01-classes_objetos/pessoa.cpp
+++ 01-classes_objetos/pessoa.cpp
@@ -13,16 +13,43 @@ class Pessoa {
 
     // Métodos da classe (comportamentos)
     public: // public: qualquer um pode acessar
-        Pessoa(){  // Construtor (inicializa os atributos)
+        // Construtor (inicializa os atributos)
+        Pessoa(){  
             nome = "";
             sexo = '\0';
             idade = 0;
             usaOculos = false;
         }
+        
+        // Retorna a idade da pessoa
+        int getIdade(){
+            return idade;
+        };
+
+        // Define a idade da pessoa com validação
+        void setIdade(int idade){
+            if(idade >= 0 && idade <= 100){
+                this->idade = idade;
+            } else {
+                cout << "Idade inválida!" << endl;
+            }
+        };
+
+        // Verifica se a pessoa pode dirigir (idade >= 18)
+        bool podeDirigir(){
+            if(idade >= 18){
+                return true;
+            } else {
+                return false;
+            }
+        };
 };
 
 int main(){
     Pessoa p1;
+    p1.setIdade(20);
+    cout << "Idade: " << p1.getIdade() << endl;
+    cout << "Pode dirigir? " << (p1.podeDirigir() ? "Sim" : "Não") << endl;
     
     return 0;
 }
