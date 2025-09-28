c2127c71d9b18b537f05b6a7ac24520e398f89c9
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-28 17:27:29 -0300

    Adiciona implementação da classe Carro com construtor padrão

---
+++ 01-classes_objetos/02-carro/carro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "carro.h"
+
+using namespace std;
+
+Carro::Carro() {
+    modelo = "Sem modelo";
+    ano = 0;
+    cor = "Sem cor";
+}
---
+++ 01-classes_objetos/02-carro/carro.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Carro {
+    private:
+        string modelo;
+        int ano;
+        string cor;
+
+    public:
+        Carro(); // Construtor padrão
+}
